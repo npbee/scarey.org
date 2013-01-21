78342739729c76bbf7b12e67251ed602140a54b3
Nick Ball <nicholaspball@gmail.com>
2013-01-21 16:55:09 -0600

    initialized susy grid, tour page setup

---
+++ _/php/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>S. Carey</title>
+    <link rel="stylesheet" href="_/css/style.css">
+    <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
