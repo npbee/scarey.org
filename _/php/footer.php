cea05b80d6987d43e77be040c3bd05359cce4478
Nick Ball <nicholaspball@gmail.com>
2013-01-21 17:16:05 -0600

    added footer styling to stick to bottom of page

---
+++ _/php/footer.php
@@ -0,0 +1,7 @@
+<footer id="footer">
+        <a href="#">facebook</a>
+        <a href="#">twitter</a>
+        <a href="#">s. carey</a>
+        <a href="#">soundcloud</a>
+        <a href="#">vimeo</a>
+</footer>
