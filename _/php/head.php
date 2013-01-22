109a6e1154588eff8199887d67bad8911c9deaff
Nick Ball <nicholaspball@gmail.com>
2013-01-21 21:50:45 -0600

    added content for tour dates, contacts, and albums

--- _/php/head.php
+++ _/php/head.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>S. Carey</title>
-    <link rel="stylesheet" href="_/css/style.css">
+    <link rel="stylesheet" href="../_/css/style.css">
     <!--[if IE]>
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
