4c9bd848046786234cc530bd16c31dcc1a540388
Nick Ball <nicholaspball@gmail.com>
2013-01-21 23:36:10 -0600

    blog image placeholders, added entypo icons, initial mobile menu setup

---
+++ _/php/nav.php
@@ -0,0 +1,10 @@
+<nav id="nav">
+	<a class="blog-nav" data-icon="&#xe000;" aria-hidden="true" href="blog">Blog</a>
+	<a class="tour-nav" data-icon="&#xe001;" aria-hidden="true" href="/tour/2010">Tour</a>
+	<a class="albums-nav" data-icon="&#xe002;" aria-hidden="true" href="/albums/lp">Albums</a>
+	<a class="logo-nav" href="/" class="logo">S. Carey</a>
+	<a class="media-nav" data-icon="&#xe003;" aria-hidden="true" href="/media/audio">Media</a>
+	<a class="store-nav" data-icon="&#xe005;" aria-hidden="true" href="store">Store</a>
+	<a class="contact-nav" data-icon="&#xe004;" aria-hidden="true" href="contact">Contact</a>
+	<a class="more-nav" data-icon="&#xe006;" aria-hidden="true" href="#">Menu</a>
+</nav>
