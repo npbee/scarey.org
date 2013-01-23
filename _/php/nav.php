50f262abe939fa76172e0c881ef7dcbafaff7d1e
Nick Ball <nicholaspball@gmail.com>
2013-01-22 22:44:26 -0600

    updated nav links, tweaked mobile navigation to remove text and keep icon, layout of in page nav

--- _/php/nav.php
+++ _/php/nav.php
@@ -1,10 +1,10 @@
 <nav id="nav">
-	<a class="blog-nav" data-icon="&#xe000;" aria-hidden="true" href="blog">Blog</a>
-	<a class="tour-nav" data-icon="&#xe001;" aria-hidden="true" href="/tour/2010">Tour</a>
-	<a class="albums-nav" data-icon="&#xe002;" aria-hidden="true" href="/albums/lp">Albums</a>
+	<a class="blog-nav" data-icon="&#xe000;" aria-hidden="true" href="/blog"><span class="hide">Blog</span></a>
+	<a class="tour-nav" data-icon="&#xe001;" aria-hidden="true" href="/tour/2010+"><span class="hide">Tour</span></a>
+	<a class="albums-nav" data-icon="&#xe002;" aria-hidden="true" href="/albums/lp"><span class="hide">Albums</span></a>
 	<a class="logo-nav" href="/" class="logo">S. Carey</a>
-	<a class="media-nav" data-icon="&#xe003;" aria-hidden="true" href="/media/audio">Media</a>
-	<a class="store-nav" data-icon="&#xe005;" aria-hidden="true" href="store">Store</a>
-	<a class="contact-nav" data-icon="&#xe004;" aria-hidden="true" href="contact">Contact</a>
-	<a class="more-nav" data-icon="&#xe006;" aria-hidden="true" href="#">Menu</a>
+	<a class="media-nav" data-icon="&#xe003;" aria-hidden="true" href="/media/audio"><span class="hide">Media</span></a>
+	<a class="store-nav" data-icon="&#xe005;" aria-hidden="true" href="/store"><span class="hide">Store</span></a>
+	<a class="contact-nav" data-icon="&#xe004;" aria-hidden="true" href="/contact"><span class="hide">Contact</span></a>
+	<a class="more-nav" data-icon="&#xe006;" aria-hidden="true" href="#"><span class="hide">Menu</span></a>
 </nav>
