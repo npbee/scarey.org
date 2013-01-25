f66688cabf37da1af5ffdc2cc928232d9457161e
Nick Ball <nicholaspball@gmail.com>
2013-01-25 08:53:58 -0600

    changed viewport meta, updated 2011 tour date html structure

--- _/php/head.php
+++ _/php/head.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>S. Carey</title>
+    <meta name="viewport" content="width=device-width">
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="../_/css/style.css">
     <!--[if IE]>
