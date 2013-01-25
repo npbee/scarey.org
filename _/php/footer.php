e3fe7c1c4dc8f8f8c76c93a25e5270650e653149
Nick Ball <nicholaspball@gmail.com>
2013-01-25 10:01:33 -0600

    new entypo social icons, updated remaining tour dates

--- _/php/footer.php
+++ _/php/footer.php
@@ -1,7 +1,7 @@
 <footer id="footer">
-        <a href="#">facebook</a>
-        <a href="#">twitter</a>
-        <a href="#">s. carey</a>
-        <a href="#">soundcloud</a>
-        <a href="#">vimeo</a>
+    <a class="facebook-footer" data-icon="&#xe008;" aria-hidden="true" href="http://facebook.com/scareymusic"><span class="hide">facebook</span></a>
+    <a class="twitter-footer" data-icon="&#xe00a;" aria-hidden="true" href="http://twitter.com/scareymusic"><span class="hide">twitter</span></a>
+    <a href="#">s. carey</a>
+    <a class="soundcloud-footer" data-icon="&#xe009;" aria-hidden="true" href="http://soundcloud.com/scareymusic"><span class="hide">soundcloud</span></a>
+    <a class="twitter-footer" data-icon="&#xe007;" aria-hidden="true" href="http://vimeo.com/scareymusic"><span class="hide">vimeo</span></a>
 </footer>
