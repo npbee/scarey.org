cf4cce2116b5c4ea38009fd66b3f3a1dddcbda1f
Nick Ball <nicholaspball@gmail.com>
2013-01-24 21:04:38 -0600

    larger grid, fixed show dates for responsive display, padding/margin updates

--- _/php/head.php
+++ _/php/head.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>S. Carey</title>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="../_/css/style.css">
     <!--[if IE]>
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
