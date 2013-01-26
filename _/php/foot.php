3ccb778897ef166f15497db863888df3761bfd06
Nick Ball <nicholaspball@gmail.com>
2013-01-26 17:07:14 -0600

    failed historyjs attempts

--- _/php/foot.php
+++ _/php/foot.php
@@ -1,5 +1,9 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script defer src="http://balupton.github.com/jquery-scrollto/scripts/jquery.scrollto.min.js"></script>
+<script defer src="../_/js/history.js"></script>
+<!-- <script defer src="../_/js/ajaxify.js"></script> -->
 <script src="../_/js/functions.js"></script>
+
 
 </body>
 </html>
--- _/php/head.php
+++ _/php/head.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>S. Carey</title>
     <meta name="viewport" content="width=device-width">
+
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="../_/css/style.css">
     <script src="../_/js/modernizr.js"></script>
@@ -11,4 +12,3 @@
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 </head>
-
