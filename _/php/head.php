49781f160a28ebd11d1237741b78c4f8cccfa8f9
Nick Ball <nicholaspball@gmail.com>
2013-01-25 18:59:18 -0600

    add soundcloud player, videos, initial image gallery tests

--- _/php/head.php
+++ _/php/head.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="../_/css/style.css">
+    <link rel="stylesheet" href="../_/flexslider.css" type="text/css">
     <!--[if IE]>
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
