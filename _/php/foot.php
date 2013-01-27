5b7a01bed0497acd5259bcba488fea4ba987d37e
Nick Ball <nicholaspball@gmail.com>
2013-01-27 13:42:51 -0600

    add wrap div to fix sticky footer

--- _/php/foot.php
+++ _/php/foot.php
@@ -1,7 +1,4 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script defer src="http://balupton.github.com/jquery-scrollto/scripts/jquery.scrollto.min.js"></script>
-<script defer src="../_/js/history.js"></script>
-<!-- <script defer src="../_/js/ajaxify.js"></script> -->
 <script src="../_/js/functions.js"></script>
 
 
--- _/php/nav.php
+++ _/php/nav.php
@@ -1,12 +1,12 @@
 <nav id="nav" class="main-nav">
-	<a class="blog-nav" data-icon="&#xe000;" aria-hidden="true" href="/blog"><span class="hide">Blog</span></a>
-	<a class="tour-nav" data-icon="&#xe001;" aria-hidden="true" href="/tour/2010+"><span class="hide">Tour</span></a>
-	<a class="albums-nav" data-icon="&#xe002;" aria-hidden="true" href="/albums/lp"><span class="hide">Albums</span></a>
-	<a class="logo-nav" href="/" class="logo">S. Carey</a>
-	<a class="media-nav" data-icon="&#xe003;" aria-hidden="true" href="/media/audio"><span class="hide">Media</span></a>
-	<a class="store-nav" data-icon="&#xe005;" aria-hidden="true" href="/store"><span class="hide">Store</span></a>
-	<a class="contact-nav" data-icon="&#xe004;" aria-hidden="true" href="/contact"><span class="hide">Contact</span></a>
-	<a class="more-nav" data-icon="&#xe006;" aria-hidden="true" href="#mobile-flyout"><span class="hide">Menu</span></a>
+	<a class="blog-nav no-ajaxy" data-icon="&#xe000;" aria-hidden="true" href="/blog"><span class="hide">Blog</span></a>
+	<a class="tour-nav no-ajaxy" data-icon="&#xe001;" aria-hidden="true" href="/tour/2010+"><span class="hide">Tour</span></a>
+	<a class="albums-nav no-ajaxy" data-icon="&#xe002;" aria-hidden="true" href="/albums/lp"><span class="hide">Albums</span></a>
+	<a class="logo-nav no-ajaxy" href="/" class="logo">S. Carey</a>
+	<a class="media-nav no-ajaxy" data-icon="&#xe003;" aria-hidden="true" href="/media/audio"><span class="hide">Media</span></a>
+	<a class="store-nav no-ajaxy" data-icon="&#xe005;" aria-hidden="true" href="/store"><span class="hide">Store</span></a>
+	<a class="contact-nav no-ajaxy" data-icon="&#xe004;" aria-hidden="true" href="/contact"><span class="hide">Contact</span></a>
+	<a class="more-nav no-ajaxy" data-icon="&#xe006;" aria-hidden="true" href="#mobile-flyout"><span class="hide">Menu</span></a>
 	<label for="main-nav-check" class="nojs-mobile-nav" data-icon="&#xe006;" aria-hidden="true" href="#mobile-flyout"><span class="hide">Close</span></label>
 </nav>
 <input type="checkbox" class="main-nav-check" id="main-nav-check" />
