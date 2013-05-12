603867c12b73b70b63462e17741117c7c0547c82
Nick Ball <nicholaspball@gmail.com>
2013-05-12 18:19:26 -0500

    working on feedback form

--- _/js/functions.js
+++ _/js/functions.js
@@ -291,6 +291,55 @@ $(document).ready(function() {
         }
     });
 
+    $("#contact").validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            message: {
+                required: true,
+                minlength: 5
+            }
+        },
+        messages: {
+            name: "Please enter your name",
+            email: "Please enter your email",
+            message: "Please enter a message"
+        }
+    });
+
+    $("#contact").submit(function(e){
+        e.preventDefault();
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var text = $("#message").val();
+        var dataString = 'name=' + name + '&email=' + email + '&text=' + message;
+        function isValidEmail(emailAddress) {
+        var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+        return pattern.test(emailAddress);
+      }
+ 
+  if (isValidEmail(email) && (message.length > 100) && (name.length > 1)){
+    $.ajax({
+    type: "POST",
+    url: "../../feedback/contactengine.php",
+    data: dataString,
+    success: function(){
+      $('.success').fadeIn(1000);
+    }
+    });
+  } else{
+    $('.error').fadeIn(1000);
+  }
+ 
+  return false;
+});
+
 });
 
 
