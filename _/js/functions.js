4f224109e92aedbe69f4009f4481553112b61200
Nick Ball <nicholaspball@gmail.com>
2013-10-02 17:39:09 -0500

    added smooth scrolling object

--- _/js/functions.js
+++ _/js/functions.js
@@ -1,4 +1,18 @@
 /* -----------------------------------------------
+SMOOTH SCROLLING
+------------------------------------------------ */
+var smoothScroll = {
+    init: function() {
+        //Localscroll
+        $.localScroll({
+            hash: true,
+            duration: '1000',
+            easing:'easeInOutExpo'
+        });
+    }
+};
+
+/* -----------------------------------------------
 MOBILE FLYOUT
 ------------------------------------------------ */
 var navFly = {
@@ -16,12 +30,26 @@ ALBUM FILTER
 ------------------------------------------------ */
 var albumFilter = {
     init: function() {
+
         $('.filter-toggle').click(function() {
             $('#slider').toggleClass("filter-show");
         });
         $('.filter-close').click(function() {
             $('#slider').removeClass("filter-show");
         });
+
+        this.navigate();
+    },
+
+    //allow for permalinks
+    navigate: function() {
+        var hash = window.location.hash,
+              $target = $(hash);
+        if ( hash ) {
+            $.scrollTo( $target, 1000, {
+                easing: 'easeInOutExpo'
+            })
+        }
     }
 };
 
@@ -198,6 +226,7 @@ HISTORY
                         navFly.init();
                         albumFilter.init();
                         slider.init();
+                        smoothScroll.init();
 
                         // Update the title
                         document.title = $data.find('.document-title:first').text();
@@ -251,13 +280,8 @@ $(document).ready(function() {
     navFly.init();
     albumFilter.init();
     slider.init();
+    smoothScroll.init();
 
-    //Localscroll
-    $.localScroll({
-        hash: true,
-        duration: '1000',
-        easing:'easeInOutExpo'
-    });
 
 
     //Colorbox (only load if desktop)
@@ -291,55 +315,6 @@ $(document).ready(function() {
             $(this).siblings().css('text-align', 'justify');
         }
     });
-
-    $("#contact").validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            message: {
-                required: true,
-                minlength: 5
-            }
-        },
-        messages: {
-            name: "Please enter your name",
-            email: "Please enter your email",
-            message: "Please enter a message"
-        }
-    });
-
-    $("#contact").submit(function(e){
-        e.preventDefault();
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var text = $("#message").val();
-        var dataString = 'name=' + name + '&email=' + email + '&text=' + message;
-        function isValidEmail(emailAddress) {
-        var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-        return pattern.test(emailAddress);
-      }
-
-  if (isValidEmail(email) && (message.length > 100) && (name.length > 1)){
-    $.ajax({
-    type: "POST",
-    url: "../../feedback/contactengine.php",
-    data: dataString,
-    success: function(){
-      $('.success').fadeIn(1000);
-    }
-    });
-  } else{
-    $('.error').fadeIn(1000);
-  }
-
-  return false;
-});
 
 });
 
