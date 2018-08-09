172e6d6babce9992be37ff1094e0e4897d650b09
Nick Ball <nicholaspball@gmail.com>
2018-08-08 18:42:29 -0700

    Purge cache

--- _site/assets/php/foot.php
+++
@@ -1,6 +0,0 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script src="../assets/js/plugins-ck.js"></script>
-<script src="../assets/js/functions.js"></script>
-
-</body>
-</html>
