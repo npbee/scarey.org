93581b8d459c25babbd157398bb0fd281a9148f5
Nick Ball <nicholaspball@gmail.com>
2020-04-07 18:30:20 -0700

    Better css tree shaking

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,26 +1,14 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
-  content: ["./src/site/**/*.{njk,md}"],
+  content: [
+    "./src/site/**/*.{njk,md}",
+    "./src/site/_includes/**/*.js",
+    "./src/*.js",
+    "./.eleventy.js"
+  ],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-  whitelist: [
-    "underline",
-    "table",
-    "thead",
-    "tbody",
-    "tr",
-    "td",
-    "th",
-    "sticky",
-    "credit",
-    "text-orange",
-    "italic",
-    "text-gray",
-    "text-center",
-    "border"
-  ],
-  whitelistPatterns: [/border/, /text-orange-400/]
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 });
 
 module.exports = {
