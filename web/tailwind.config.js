3d7b0cf539fa1155c9c8ae6d3d970cee3a5e6011
Nick Ball <nicholaspball@gmail.com>
2020-10-03 15:40:02 -0700

    Upgrade tailwind

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,9 +1,19 @@
 module.exports = {
+  purge: [
+    "./src/site/**/*.{njk,md}",
+    "./src/site/_includes/**/*.js",
+    "./src/*.js",
+    "./.eleventy.js"
+  ],
   theme: {
     screens: {
       sm: "640px",
       md: "768px",
       lg: "1024px"
     }
+  },
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
   }
 };
