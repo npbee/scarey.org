06d78e3a13c7a03ec00995de4754780e0844bea2
Nick Ball <nicholaspball@gmail.com>
2021-11-20 09:39:03 -0800

    Upgrade cssnano

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,3 +1,12 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")]
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    isProd &&
+      require("cssnano")({
+        preset: "default",
+      }),
+  ],
 };
